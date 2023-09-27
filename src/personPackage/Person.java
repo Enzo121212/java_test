bb6c36d2994db149ccd9e9d2509dca1f608b0d25
Enzo <anjaratox@yahoo.fr>
2023-09-27 20:00:38 +0300

    first commit

---
+++ src/personPackage/Person.java
@@ -0,0 +1,54 @@
+package personPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.org.apache.xml.internal.serialize.Printer;
+
+/**
+ * Servlet implementation class Person
+ */
+@WebServlet("/Person")
+public class Person extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Person() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/**response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("<h1> Bonjour je suis la pour vous benir </h1>");*/
+		
+		String nom = "Faly mahita anao zah malala aa";
+		
+		request.setAttribute("nom", nom);
+		
+		this.getServletContext().getRequestDispatcher("/Person.jsp").forward(request, response);;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
